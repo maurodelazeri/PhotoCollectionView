b6e61194fd7775e22cf8b2fc59ee19b421831c3f
sarbogast <sebastien@instaply.com>
2014-12-23 22:07:09 +0100

    Initial Commit

---
+++ CollectionViewTutorial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CollectionViewTutorial
+//
+//  Created by Sebastien Arbogast on 23/12/2014.
+//  Copyright (c) 2014 Epseelon sprl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
